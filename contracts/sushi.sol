af26189dbacd690b15dcb3926d935eb659753e57
ankhaa14 <ankhjargal.614@gmail.com>
2025-04-01 21:21:33 -0400

    Added sushiswap contract

--- contracts/sushi.sol
+++ contracts/sushi.sol
@@ -20,7 +20,6 @@ contract SushiswapDataFetcher {
     address public weth;
     address public usdc;
 
-    // Initialize the contract with the addresses for the Sushiswap factory, WETH, and USDC
     constructor(
         address _factory,
         address _weth,
@@ -31,20 +30,16 @@ contract SushiswapDataFetcher {
         usdc = _usdc;
     }
 
-    // Returns the estimated amount of USDC for a given amount of WETH input
     function getWethToUsdcAmountOut(uint amountIn) external view returns (uint amountOut) {
-        // Get the pair address for WETH and USDC from the factory
         address pairAddress = IUniswapV2Factory(factory).getPair(weth, usdc);
         require(pairAddress != address(0), "Pair not found");
 
-        // Get reserves from the pair contract
         (uint112 reserve0, uint112 reserve1, ) = IUniswapV2Pair(pairAddress).getReserves();
         address token0 = IUniswapV2Pair(pairAddress).token0();
         address token1 = IUniswapV2Pair(pairAddress).token1();
 
         uint reserveIn;
         uint reserveOut;
-        // Determine which reserve corresponds to WETH
         if (token0 == weth) {
             reserveIn = reserve0;
             reserveOut = reserve1;
@@ -55,8 +50,6 @@ contract SushiswapDataFetcher {
             revert("WETH not found in pair");
         }
 
-        // Uniswap (and Sushiswap) use the constant product formula with a fee:
-        // amountOut = (amountIn * 997 * reserveOut) / (reserveIn * 1000 + amountIn * 997)
         uint amountInWithFee = amountIn * 997;
         amountOut = (amountInWithFee * reserveOut) / (reserveIn * 1000 + amountInWithFee);
     }
